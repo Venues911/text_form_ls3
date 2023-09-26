d50c45267de815a58d5818609a7a780f2aa59cfd
IvP <vanusezzz@gmail.com>
2023-09-26 19:09:45 +0300

    examples css & xpath

---
+++ src/test/java/CssAndXpathExamples.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selenide.*;
+import static org.openqa.selenium.By.xpath;
+
+public class CssAndXpathExamples {
+    @Test
+    void CssAndXpathExamples() {
+            $("[name=q]");
+            $("testarea[name=q]"); //css-selector
+
+            $x("//textarea[@name='q']");
+            $(xpath("//textarea[@name='q']"));  // xpath-selector
+
+            $(byName("q"));
+
+
+
+    }
+}
