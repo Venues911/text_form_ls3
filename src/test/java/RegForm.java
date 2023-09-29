6bee5fa332f78fd15f1ac58639b73869b99d6e59
IvP <vanusezzz@gmail.com>
2023-09-29 20:19:41 +0300

    Upload picture func + set hobbies

--- src/test/java/RegForm.java
+++ src/test/java/RegForm.java
@@ -2,7 +2,10 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -24,7 +27,7 @@ public class RegForm {
         $("#lastName").setValue("AN");
         $("#userEmail").setValue("vanusezzz@gmail.com");
 
-        $("#gender-radio-1").parent().click(); // or $(byText("Male")).click();
+        $("#gender-radio-1").parent().click(); // or .$(byText("Male")).click();
         $("#userNumber").setValue("+79997998872");
 
 
@@ -32,15 +35,12 @@ public class RegForm {
         $(".react-datepicker__month-select").selectOption("September"); // OR .selectOptionByValue("9");
         $(".react-datepicker__year-select").selectOption("1997");
         $(".react-datepicker__day--002:not(.react-datepicker__day--outside-month)").click();
-        $("#subjectsContainer").setValue("Some address");
+        $("#subjectsInput").setValue("Math").pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+
+//        $("#uploadPicture").uploadFile(new File("src/test/resources/img/1.png"));
+        $("#uploadPicture").uploadFromClasspath ("img/1.png");
         $("#currentAddress").setValue("Some address 1");
 
-//        $("#submit").click();
-//        //check values in fill
-//        $("#output").$("#name").shouldHave(text("IP"));
-//        $("#output #name").shouldHave(text("IP"));
-//        $("#output #email").shouldHave(text("vanusezzz@gmail.com"));
-//        $("#output #currentAddress").shouldHave(text("Some address 1"));
-//        $("#output #permanentAddress").shouldHave(text("Another address 1"));
     }
 }
