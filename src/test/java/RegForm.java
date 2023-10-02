ef341acc79edf697ad11c6e0a486816e5e375ce6
IvP <vanusezzz@gmail.com>
2023-10-02 21:44:56 +0300

    //        Configuration.holdBrowserOpen=true;

--- src/test/java/RegForm.java
+++ src/test/java/RegForm.java
@@ -17,7 +17,7 @@ public class RegForm {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen=true;
+//        Configuration.holdBrowserOpen=true;
 
     }
 
